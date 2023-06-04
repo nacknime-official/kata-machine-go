2def737764840dd85e82f40f06a370b3f49677ee
Riccardo Roman <riccardo.riccardoroman@gmail.com>
2023-06-05 01:24:16 +0200

    feat: add `ArraysEqual` func

--- src/DSA/helpers.go
+++ src/DSA/helpers.go
@@ -4,3 +4,17 @@ func ZeroValue[T any]() T {
 	var zero T
 	return zero
 }
+
+func ArraysEqual(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
